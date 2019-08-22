6337dc54a68761c3a5af725514bef3d8c8704455
Diana Grams <deegrams221@gmail.com>
2019-08-22 10:08:06 -0600

    completed step 2 - Validation and Erros

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
 
-function LoginForm() {
+function LoginForm({errors, touched, values}) {
   return(
     <Form>
       <div>
+        {/* step 2: Erros  */}
+        {errors.name && touched.name && <p>{errors.name}</p>}
         <Field type="text" name="name" placeholder="Name" />
       </div>
       <div>
+        {errors.email && touched.email && <p>{errors.email}</p>}
         <Field type="email" name="email" placeholder="Email" />
       </div>
       <div>
+        {errors.password && touched.password && <p>{errors.password}</p>}  
         <Feild type="password" name="password" placeholder="Password" />
       </div>
       <label>
@@ -31,6 +36,19 @@ const FormikLoginForm = withFormik ({
         tos: tos || false
       };
   },
+
+  // Step 2: Validation Schema
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .min(2, "Must enter a name")
+      .required("Name is required"),
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required")      
+  }),
 
   handleSubmit(values) {
     console.log(values);
